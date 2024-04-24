ca39ca41f0f3e50ad1b672217942f5a89905e5ca
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-04-24 23:03:51 +0200

    add themes

---
+++ components/ThemeChanger.js
@@ -0,0 +1,31 @@
+"use client";
+import { SunIcon, MoonIcon } from "@heroicons/react/16/solid";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function ThemeChanger() {
+  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return
+  }
+
+  return (
+    <button
+      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+      aria-label="Toggle Dark Mode"
+      className="flex justify-center items-center cursor-pointer group duration-150 text-gray-800 dark:text-gray-200 p-2"
+    >
+      {theme === "light" ? (
+        <SunIcon className="h-4 w-4 stroke-2  group-hover:scale-110 duration-150" />
+      ) : (
+        <MoonIcon className="h-4 w-4 stroke-2  group-hover:scale-110 duration-150" />
+      )}
+    </button>
+  );
+}
