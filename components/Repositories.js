24d4405c455d10ed600229fb24119b143ff579ae
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-04-24 23:04:02 +0200

    add repos

---
+++ components/Repositories.js
@@ -0,0 +1,90 @@
+import Link from "next/link";
+
+const lang = [
+  {
+    name: "JavaScript",
+    color: "#f1e05a",
+  },
+  {
+    name: "Python",
+    color: "#3572b0",
+  },
+  {
+    name: "C++",
+    color: "#f34b7d",
+  },
+  {
+    name: "Java",
+    color: "#b07219",
+  },
+  {
+    name: "PHP",
+    color: "#4f5d95",
+  },
+  {
+    name: "C#",
+    color: "#178600",
+  },
+  {
+    name: "Ruby",
+    color: "#701516",
+  },
+  {
+    name: "Go",
+    color: "#00add8",
+  },
+];
+
+export default async function Repositories() {
+  const res = await fetch(
+    "https://api.github.com/users/R4ULtv/repos?sort=updated&per_page=5",
+    { next: { revalidate: 604800 } } // Revalidate every week
+  );
+  const data = await res.json();
+
+  return (
+    <div className="flex flex-col gap-7 sm:gap-4">
+      {data &&
+        data.map((repo) => (
+          <Link
+            href={repo.html_url}
+            key={repo.id}
+            target="_blank"
+            className="-mx-3 flex items-start gap-1.5 rounded-md px-3 no-underline sm:py-3 hover:bg-zinc-200 dark:hover:bg-zinc-800 duration-150"
+          >
+            <div className="relative flex h-2 w-2 mt-2">
+              <span
+                className="animate-ping absolute inline-flex h-full w-full rounded-full opacity-75"
+                style={{
+                  backgroundColor: lang.find((l) => l.name === repo.language)
+                    ?.color,
+                }}
+              ></span>
+              <span
+                className="relative inline-flex rounded-full h-2 w-2 opacity-90"
+                style={{
+                  backgroundColor: lang.find((l) => l.name === repo.language)
+                    ?.color,
+                }}
+              ></span>
+            </div>
+            <div className="flex flex-col">
+              <span className="font-medium text-zinc-800 dark:text-zinc-200">
+                {repo.name}{" "}
+                <span className="font-normal">
+                  •{" "}
+                  {new Date(repo.created_at).toLocaleDateString("en-US", {
+                    month: "short",
+                    year: "numeric",
+                  })}
+                </span>
+              </span>
+              <span className="text-gray-600 dark:text-zinc-400">
+                {repo.description ? repo.description : "No description."}
+              </span>
+            </div>
+          </Link>
+        ))}
+    </div>
+  );
+}
