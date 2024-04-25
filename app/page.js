4291424ef8bc670f321890e8ae821a7e73e4ca83
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-04-25 18:31:01 +0200

    add about

--- app/page.js
+++ app/page.js
@@ -1,15 +1,62 @@
 import Repositories from "@/components/Repositories";
 import Link from "next/link";
 
-export const dynamic = 'force-static';
+export const dynamic = "force-static";
 
 export default function Home() {
   return (
     <main className="space-y-20">
       <div>
         <Link
+          href={"#about"}
+          className="py-5 sm:py-4 block font-medium text-zinc-800 dark:text-zinc-200 group"
+          id="about"
+        >
+          About{" "}
+          <span className="opacity-0 group-hover:opacity-90 duration-150">
+            #
+          </span>
+        </Link>
+        <p className="text-zinc-600 dark:text-zinc-400">
+          Hi! I'm a full-stack developer based in Milan, currently juggling
+          projects and studies. On the professional side, I'm diving into
+          exciting ventures like{" "}
+          <Link
+            href="https://www.shrly.cc/"
+            target="_blank"
+            className="underline-offset-2 underline hover:text-zinc-500"
+          >
+            Shortly
+          </Link>{" "}
+          and the{" "}
+          <Link
+            href="https://www.shrly.cc/"
+            target="_blank"
+            className="underline-offset-2 underline hover:text-zinc-500"
+          >
+            PedeBeats
+          </Link>{" "}
+          e-commerce platform.
+        </p>
+        <p className="text-zinc-600 dark:text-zinc-400 mt-2">
+          My true passion lies in crafting websites that solve problems. But
+          there's more to me than code! When I'm not building, music takes
+          center stage. I love producing tracks and experimenting with fresh
+          sounds. If you are interested in my music check{" "}
+          <Link
+            href="/r/spotify"
+            target="_blank"
+            className="underline-offset-2 underline hover:text-zinc-500"
+          >
+            here
+          </Link>
+          .
+        </p>
+      </div>
+      <div>
+        <Link
           href={"#projects"}
-          className="mb-5 block font-medium sm:mb-4 text-zinc-800 dark:text-zinc-200 group"
+          className="py-5 sm:py-4 block font-medium text-zinc-800 dark:text-zinc-200 group"
           id="projects"
         >
           Projects{" "}
@@ -26,7 +73,7 @@ export default function Home() {
             <span className="font-medium text-zinc-800 dark:text-zinc-200">
               PedeBeats <span className="font-normal">• Dec 2023</span>
             </span>
-            <span className="text-gray-600 dark:text-zinc-400">
+            <span className="text-zinc-600 dark:text-zinc-400">
               E-Commerce website for selling music production kits.
             </span>
           </Link>
@@ -39,7 +86,7 @@ export default function Home() {
             <span className="font-medium text-zinc-800 dark:text-zinc-200">
               Shortly <span className="font-normal">• Aug 2023</span>
             </span>
-            <span className="text-gray-600 dark:text-zinc-400">
+            <span className="text-zinc-600 dark:text-zinc-400">
               URL shortener website built with Next.js and Vercel, with
               analytics.
             </span>
@@ -53,7 +100,7 @@ export default function Home() {
             <span className="font-medium text-zinc-800 dark:text-zinc-200">
               BeatsLab <span className="font-normal">• Jun 2023</span>
             </span>
-            <span className="text-gray-600 dark:text-zinc-400">
+            <span className="text-zinc-600 dark:text-zinc-400">
               A website that sells beats and kits for producers and rappers.
             </span>
           </Link>
@@ -62,7 +109,7 @@ export default function Home() {
       <div>
         <Link
           href={"#repositories"}
-          className="mb-5 block font-medium sm:mb-4 text-zinc-800 dark:text-zinc-200 group"
+          className="py-5 sm:py-4 block font-medium text-zinc-800 dark:text-zinc-200 group"
           id="repositories"
         >
           Repositories{" "}
