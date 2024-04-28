25b66c6b53c60017def56e55779723cdcf4578d7
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-04-28 11:02:00 +0200

    moved in api route

---
+++ app/api/dynamic-og/route.js
@@ -0,0 +1,45 @@
+import { ImageResponse } from "next/og";
+export const runtime = "edge";
+
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const title = searchParams.get("title") || "Raul Carini";
+  const description = searchParams.get("description") || "Software Engineer";
+
+  const fontData = await fetch(
+    new URL("/assets/Geist-Black.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      <div tw="h-full w-full flex flex-col justify-center items-center p-14 bg-zinc-100">
+        <div tw="flex absolute top-10 right-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        <span tw="text-center text-8xl text-zinc-800">{title}</span>
+        <span tw="mt-6 text-center text-4xl text-zinc-700">{description}</span>
+      </div>
+    ),
+    {
+      width: 843,
+      height: 441,
+      fonts: [
+        {
+          name: "GeistVF",
+          data: fontData,
+          style: "normal",
+        },
+      ],
+    }
+  );
+}
