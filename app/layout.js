90a549d896d950a6fd6c3ccdeb98e98978fe87af
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-04-28 11:08:26 +0200

    changed vercel_url to host_name

--- app/layout.js
+++ app/layout.js
@@ -9,9 +9,7 @@ import ThemeChanger from "@/components/ThemeChanger";
 import { ViewTransitions } from "next-view-transitions";
 
 export const metadata = {
-  metadataBase: process.env.VERCEL_URL
-    ? new URL(`https://${process.env.VERCEL_URL}`)
-    : new URL(`http://localhost:${process.env.PORT || 3000}`),
+  metadataBase: process.env.HOST_NAME,
   title: "Raul Carini",
   description:
     "Full Stack Developer working on solo projects and freelancing, currently based in Milan, Italy",
@@ -19,12 +17,10 @@ export const metadata = {
     title: "Raul Carini",
     description:
       "Full Stack Developer working on solo projects and freelancing, currently based in Milan, Italy",
-    url: process.env.VERCEL_URL
-      ? new URL(`https://${process.env.VERCEL_URL}`)
-      : new URL(`http://localhost:${process.env.PORT || 3000}`),
+    url: process.env.HOST_NAME,
     images: [
       {
-        url: `https://${process.env.VERCEL_URL}/api/dynamic-og`,
+        url: `${process.env.HOST_NAME}/api/dynamic-og`,
         width: 843,
         height: 441,
       },
