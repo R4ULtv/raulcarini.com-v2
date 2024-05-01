008101bfe5440935798f6a5974bb407edf424c73
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-01 18:21:46 +0200

    removed draggable globe

--- components/GlobeStasts.js
+++ components/GlobeStasts.js
@@ -2,22 +2,12 @@
 
 import createGlobe from "cobe";
 import { useEffect, useRef } from "react";
-import { useSpring } from "react-spring";
 
 export default function GlobeStasts({ locations }) {
   const canvasRef = useRef();
-  const pointerInteracting = useRef(null);
-  const pointerInteractionMovement = useRef(0);
-  const [{ r }, api] = useSpring(() => ({
-    r: 0,
-    config: {
-      mass: 1,
-      tension: 280,
-      friction: 40,
-      precision: 0.001,
-    },
-  }));
   useEffect(() => {
+    const theme = window.localStorage.getItem("theme") || "light";
+
     let phi = 0;
     let width = 0;
     const onResize = () =>
@@ -29,22 +19,27 @@ export default function GlobeStasts({ locations }) {
       width: width * 2,
       height: width * 2,
       phi: 0,
-      theta: 0.1,
-      dark: 1,
+      theta: 0.2,
+      dark: theme === "dark" ? 1 : 0,
       diffuse: 1,
       mapSamples: 30000,
-      mapBrightness: 12,
-      baseColor: [39 / 255, 39 / 255, 42 / 255],
+      mapBrightness: 8,
+      baseColor:
+        theme === "dark"
+          ? [39 / 255, 39 / 255, 42 / 255]
+          : [255 / 255, 255 / 255, 255 / 255],
       markerColor: [251 / 255, 100 / 255, 21 / 255],
-      glowColor: [39 / 255, 39 / 255, 42 / 255],
+      glowColor:
+        theme === "dark"
+          ? [39 / 255, 39 / 255, 42 / 255]
+          : [255 / 255, 255 / 255, 255 / 255],
       markers: locations,
-      opacity: 0.9,
-
+      opacity: 0.95,
       onRender: (state) => {
-        if (!pointerInteracting.current) {
-          phi += 0.005;
-        }
-        state.phi = phi + r.get();
+        // Called on every animation frame.
+        // `state` will be an empty object, return updated params.
+        state.phi = phi;
+        phi += 0.004;
         state.width = width * 2;
         state.height = width * 2;
       },
@@ -55,55 +50,13 @@ export default function GlobeStasts({ locations }) {
       window.removeEventListener("resize", onResize);
     };
   }, []);
+
   return (
-    <div
-      style={{
-        width: "100%",
-        maxWidth: 720,
-        aspectRatio: 1,
-        margin: "auto",
-        position: "relative",
-      }}
-    >
+    <div className="w-full h-full relative m-auto aspect-square">
       <canvas
         ref={canvasRef}
-        onPointerDown={(e) => {
-          pointerInteracting.current =
-            e.clientX - pointerInteractionMovement.current;
-          canvasRef.current.style.cursor = "grabbing";
-        }}
-        onPointerUp={() => {
-          pointerInteracting.current = null;
-          canvasRef.current.style.cursor = "grab";
-        }}
-        onPointerOut={() => {
-          pointerInteracting.current = null;
-          canvasRef.current.style.cursor = "grab";
-        }}
-        onMouseMove={(e) => {
-          if (pointerInteracting.current !== null) {
-            const delta = e.clientX - pointerInteracting.current;
-            pointerInteractionMovement.current = delta;
-            api.start({
-              r: delta / 200,
-            });
-          }
-        }}
-        onTouchMove={(e) => {
-          if (pointerInteracting.current !== null && e.touches[0]) {
-            const delta = e.touches[0].clientX - pointerInteracting.current;
-            pointerInteractionMovement.current = delta;
-            api.start({
-              r: delta / 100,
-            });
-          }
-        }}
+        className="w-full h-full opacity-0"
         style={{
-          width: "100%",
-          height: "100%",
-          cursor: "grab",
-          contain: "layout paint size",
-          opacity: 0,
           transition: "opacity 1s ease",
         }}
       />
