965310b0fee64796649f8c63fe90fe6ae6d09404
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-02 19:06:28 +0200

    fix title

--- components/TopNavPost.js
+++ components/TopNavPost.js
@@ -1,9 +1,14 @@
 import moment from "moment";
+import Link from "next/link";
 
 export default function TopNavPost({ title, date, slug }) {
   return (
     <>
-      <h1 className="mb-1">{title}</h1>
+      <h1 className="mb-1">
+        <Link href="" className="font-bold">
+          {title}
+        </Link>
+      </h1>
 
       <span className="text-sm mb-1.5 flex items-center gap-1">
         {date.toLocaleDateString("en-US", {
