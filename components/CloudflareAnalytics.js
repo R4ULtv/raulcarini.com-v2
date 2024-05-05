dc5774f410829dfb3c13d7b8fe7566098950e42d
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-05 13:50:19 +0200

    moved analytics

---
+++ components/CloudflareAnalytics.js
@@ -0,0 +1,47 @@
+export default async function CloudflareAnalytics() {
+    // Define the GraphQL query to fetch data from the Cloudflare API.
+  const query = `{
+    viewer {
+        zones(filter: { zoneTag: "${process.env.CLOUDFLARE_ZONE_TAG}" }) {
+            httpRequests1dGroups(
+                filter: { date_geq: "${
+                  new Date(new Date().setDate(new Date().getDate() - 7))
+                    .toISOString()
+                    .split("T")[0]
+                }", date_leq: "${new Date().toISOString().split("T")[0]}" }
+                limit: 1
+            ) {
+                dimensions {
+                    date
+                }
+                sum {
+                    requests
+                    pageViews
+                    countryMap {
+                        clientCountryName
+                        requests
+                    }
+                }
+                uniq {
+                    uniques
+                }
+            }
+        }
+    }
+}`;
+
+  const ref = await fetch("https://api.cloudflare.com/client/v4/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`,
+    },
+    body: JSON.stringify({ query: query }),
+    next: { revalidate: 60 * 60 * 24 },
+  });
+
+  // Parse the response from the API as JSON and extract the data.
+  const data = await ref.json();
+
+  return data;
+}
