6bab4adf41a31a389b6eb0f0f4e1a9be55a14f45
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-05 13:50:07 +0200

    add metadata

--- app/stats/page.js
+++ app/stats/page.js
@@ -1,50 +1,31 @@
+import CloudflareAnalytics from "@/components/CloudflareAnalytics";
 import GlobeStasts from "@/components/GlobeStasts";
 import { search } from "country-js";
 
+export const metadata = {
+  metadataBase: process.env.HOST_NAME,
+  title: "Stats - Raul Carini",
+  description:
+    "Statistics page of my website. View the globe with the location of the latest requests from the last week. The stats are managed by CloudFlare.",
+  openGraph: {
+    title: "Stats - Raul Carini",
+    description:
+      "Statistics page of my website. View the globe with the location of the latest requests from the last week. The stats are managed by CloudFlare.",
+    url: process.env.HOST_NAME,
+    images: [
+      {
+        url: `${process.env.HOST_NAME}/api/dynamic-og?title=Statistics&description=A%20globe%20with%20the%20last%20week%20requests`,
+        width: 843,
+        height: 441,
+      },
+    ],
+  },
+};
+
 export default async function Stats() {
-  // Define the GraphQL query to fetch data from the Cloudflare API.
-  const query = `{
-    viewer {
-        zones(filter: { zoneTag: "${process.env.CLOUDFLARE_ZONE_TAG}" }) {
-            httpRequests1dGroups(
-                filter: { date_geq: "${
-                  new Date(new Date().setDate(new Date().getDate() - 7))
-                    .toISOString()
-                    .split("T")[0]
-                }", date_leq: "${new Date().toISOString().split("T")[0]}" }
-                limit: 1
-            ) {
-                dimensions {
-                    date
-                }
-                sum {
-                    requests
-                    pageViews
-                    countryMap {
-                        clientCountryName
-                        requests
-                    }
-                }
-                uniq {
-                    uniques
-                }
-            }
-        }
-    }
-}`;
+  const data = await CloudflareAnalytics();
+  if (!data) return;
 
-  const ref = await fetch("https://api.cloudflare.com/client/v4/graphql", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`,
-    },
-    body: JSON.stringify({ query: query }),
-    next: { revalidate: 60 * 60 * 24 },
-  });
-
-  // Parse the response from the API as JSON and extract the data.
-  const data = await ref.json();
   const contryMap =
     data.data.viewer.zones[0].httpRequests1dGroups[0].sum.countryMap;
 
