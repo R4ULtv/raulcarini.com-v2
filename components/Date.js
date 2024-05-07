11e36603a1dc07511af9252559e414e6ef916adf
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-07 17:19:51 +0200

    date client component

---
+++ components/Date.js
@@ -0,0 +1,16 @@
+"use client";
+
+import moment from "moment";
+
+export default function Date({ date }) {
+  return (
+    <span className="text-sm flex items-center gap-1">
+      {date.toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      })}{" "}
+      ({moment(date).fromNow()})
+    </span>
+  );
+}
