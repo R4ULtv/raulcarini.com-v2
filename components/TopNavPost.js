f03325a555201b265f9da1de1ed6d059cb61820c
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-13 16:50:24 +0200

    removed link

--- components/TopNavPost.js
+++ components/TopNavPost.js
@@ -6,11 +6,7 @@ import { Suspense } from "react";
 export default function TopNavPost({ title, date, path }) {
   return (
     <>
-      <h1 className="mb-1">
-        <Link href="" className="font-bold">
-          {title}
-        </Link>
-      </h1>
+      <h1 className="mb-1 font-bold">{title}</h1>
 
       <div className="flex items-center justify-between mb-1.5">
         <Date date={date} />
