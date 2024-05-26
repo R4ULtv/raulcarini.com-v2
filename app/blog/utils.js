b06ea98fbeeaea92bb0b2562b3d823394533b631
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-26 17:26:28 +0200

    add custom link + header

--- app/blog/utils.js
+++ app/blog/utils.js
@@ -2,10 +2,9 @@ import path from "path";
 import fs from "fs";
 import { MDXRemote } from "next-mdx-remote/rsc";
 import Image from "next/image";
+import { createElement } from "react";
+import Link from "next/link";
 
-import rehypeSlug from "rehype-slug";
-import remarkGfm from "remark-gfm";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeShiki from "@shikijs/rehype";
 
 function parseFrontmatter(fileContent) {
@@ -60,9 +59,67 @@ function CustomImage(props) {
   return <Image {...props} />;
 }
 
+function CustomLink(props) {
+  let href = props.href;
+
+  if (href.startsWith("/")) {
+    return (
+      <Link href={href} {...props}>
+        {props.children}
+      </Link>
+    );
+  }
+
+  if (href.startsWith("#")) {
+    return <a {...props} />;
+  }
+
+  return <a target="_blank" rel="noopener noreferrer" {...props} />;
+}
+
+function slugify(str) {
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
+    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
+}
+
+function createHeading(level) {
+  const Heading = ({ children }) => {
+    let slug = slugify(children);
+    return createElement(
+      `h${level}`,
+      { id: slug },
+      [
+        createElement("a", {
+          href: `#${slug}`,
+          key: `link-${slug}`,
+          className: "anchor",
+        }),
+      ],
+      children
+    );
+  };
+
+  Heading.displayName = `Heading${level}`;
+
+  return Heading;
+}
+
 export function CustomMDX(props) {
   const components = {
+    h1: createHeading(1),
+    h2: createHeading(2),
+    h3: createHeading(3),
+    h4: createHeading(4),
+    h5: createHeading(5),
+    h6: createHeading(6),
     Image: CustomImage,
+    a: CustomLink,
   };
 
   return (
@@ -71,10 +128,7 @@ export function CustomMDX(props) {
       components={{ ...components, ...(props.components || {}) }}
       options={{
         mdxOptions: {
-          remarkPlugins: [remarkGfm],
           rehypePlugins: [
-            rehypeSlug,
-            [rehypeAutolinkHeadings, { behavior: "wrap" }],
             [
               rehypeShiki,
               {
