39ae8befa5aebabd960b1eb96d3128048dff5aed
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-26 14:18:49 +0200

    dynamic sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,22 +1,20 @@
+import { getBlogPosts } from "@/app/blog/utils";
+
 export default function sitemap() {
-    return [
-      {
-        url: 'https://www.raulcarini.dev',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 1,
-      },
-      {
-        url: 'https://www.raulcarini.dev/posts/shortly',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: 'https://www.raulcarini.dev/posts/multi-region-r2-bucket-system',
-        lastModified: new Date(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-    ]
-  }
+  const allBlogs = getBlogPosts();
+
+  return [
+    {
+      url: "https://www.raulcarini.dev",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    ...allBlogs.map((blog) => ({
+      url: `https://www.raulcarini.dev/blog/${blog.slug}`,
+      lastModified: new Date(blog.metadata.createdAt),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    })),
+  ];
+}
