50248884503fb36d9ce29194faf23c812b9e0043
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-05-28 13:45:55 +0200

    add bash

--- components/mdx.js
+++ components/mdx.js
@@ -70,6 +70,7 @@ async function CustomCode({ language, code }) {
     langs: [
       import("shiki/langs/javascript.mjs"),
       import("shiki/langs/json.mjs"),
+      import("shiki/langs/bash.mjs"),
     ],
     loadWasm: getWasm,
   });
