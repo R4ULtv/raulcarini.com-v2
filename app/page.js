ab64d6b89c9596e6e6464ddeac385c2bf8816ba3
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-06-24 11:13:27 +0200

    moved projects

--- app/page.js
+++ app/page.js
@@ -1,30 +1,11 @@
 import Repositories from "@/components/Repositories";
 import Link from "next/link";
 import { getBlogPosts } from "@/app/blog/utils";
-import { ArrowRightCircleIcon, ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
-
-const projects = [
-  {
-    name: "PedeBeats",
-    description: "E-Commerce website for selling music production kits.",
-    date: new Date("2023-12-12"),
-    link: "https://kits.pedebeats.com",
-  },
-  {
-    name: "Shortly",
-    description:
-      "URL shortener website built with Next.js and Vercel, with analytics.",
-    date: new Date("2023-08-20"),
-    link: "https://www.shrly.cc",
-  },
-  {
-    name: "Beatslab",
-    description:
-      "A website that sells beats and kits for producers and rappers.",
-    date: new Date("2023-06-18"),
-    link: "https://pewview.beatslab.net",
-  },
-];
+import {
+  ArrowRightCircleIcon,
+  ArrowTopRightOnSquareIcon,
+} from "@heroicons/react/24/outline";
+import { projects } from "@/components/utils/projects";
 
 export default function Home() {
   const allBlogs = getBlogPosts();
