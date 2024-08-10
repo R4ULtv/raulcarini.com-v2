bb9bdda5b4f4e26d1488f04f00853ef7fcffdf43
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-08-10 10:42:56 +0200

    removed

--- app/api/geolocation/route.js
+++
@@ -1,7 +0,0 @@
-import { geolocation } from "@vercel/functions";
-import { NextResponse } from "next/server";
-
-export function GET(request) {
-  const details = geolocation(request) || "Unknown";
-  return NextResponse.json(details);
-}
--- app/api/ip-address/route.js
+++
@@ -1,7 +0,0 @@
-import { ipAddress } from "@vercel/functions";
-import { NextResponse } from "next/server";
-
-export function GET(request) {
-  const ip = ipAddress(request) || "Unknown";
-  return NextResponse.json({ ip });
-}
