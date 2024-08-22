9e80d5896ffd5f52e5ceb2f538c7cf18ccfa561b
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-08-22 15:20:03 +0200

    fix colors

--- components/BackToTop.js
+++ components/BackToTop.js
@@ -1,16 +1,14 @@
 "use client";
 
-import { ChevronUpIcon } from "@heroicons/react/24/outline";
+import { ChevronUpIcon } from "@heroicons/react/20/solid";
 import { useEffect, useState } from "react";
 
 export default function BackToTop() {
   const [showBackToTop, setShowBackToTop] = useState(false);
-  const [scrollPosition, setScrollPosition] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const position = window.scrollY;
-      setScrollPosition(position);
       setShowBackToTop(position > 1000);
     };
 
@@ -24,10 +22,11 @@ export default function BackToTop() {
   return (
     <button
       onClick={() => window.scrollTo({ top: 0 })}
+      aria-label="Back to top"
       style={{ display: showBackToTop ? "block" : "none" }}
-      className="fixed bottom-0 right-0 z-50 bg-zinc-700 dark:bg-zinc-300 p-1.5 m-1.5 rounded-md"
+      className="fixed bottom-0 right-0 z-50 border border-zinc-200 dark:border-zinc-800 rounded-md backdrop-blur-2xl p-0.5 m-1.5"
     >
-      <ChevronUpIcon className="h-5 w-5 text-zinc-300 dark:text-zinc-700 stroke-2" />
+      <ChevronUpIcon className="size-6 text-zinc-600 dark:text-zinc-400" />
     </button>
   );
 }
