a4e9dc14dd0b115ca2f3788c4b2e795ef5bb1b22
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-13 11:51:48 +0200

    changed sort by

--- components/utils/getRepositories.js
+++ components/utils/getRepositories.js
@@ -1,6 +1,6 @@
 export default async function getRepositories() {
   const res = await fetch(
-    "https://api.github.com/users/R4ULtv/repos?sort=updated",
+    "https://api.github.com/users/R4ULtv/repos?sort=created",
     { next: { revalidate: 604800 } } // Revalidate every week
   );
   return await res.json();
