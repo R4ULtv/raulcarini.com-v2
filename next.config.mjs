5b5a59d24358010f87774ec5d645d7d9c468cd3a
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-24 19:38:44 +0200

    removed redirects

--- next.config.mjs
+++ next.config.mjs
@@ -21,36 +21,6 @@ const nextConfig = {
         destination: "/#blog",
         permanent: true,
       },
-      {
-        source: "/r/youtube",
-        destination: "https://www.youtube.com/@lil-poop",
-        permanent: true,
-      },
-      {
-        source: "/r/twitter",
-        destination: "https://twitter.com/lil_poop__",
-        permanent: true,
-      },
-      {
-        source: "/r/github",
-        destination: "https://github.com/R4ULtv",
-        permanent: true,
-      },
-      {
-        source: "/r/instagram",
-        destination: "https://www.instagram.com/lil.poop__/",
-        permanent: true,
-      },
-      {
-        source: "/r/spotify",
-        destination: "https://open.spotify.com/artist/21DbatkhkYpxIpLfQ4LeUo",
-        permanent: true,
-      },
-      {
-        source: "/g/:path*",
-        destination: "https://github.com/R4ULtv/:path*",
-        permanent: false,
-      },
     ];
   },
 };
