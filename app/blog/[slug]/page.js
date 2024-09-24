3098f8e6e7afd5a82665076f71f221ca2c0dcf77
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-09-24 19:35:42 +0200

    fix date check

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -51,12 +51,12 @@ export default function Blog({ params }) {
 
       <div className="flex items-center justify-between mb-1.5">
         <FormattedDate date={new Date(post.metadata.createdAt)} />
-        {post.content !== "" ||
-          (post.createdAt < new Date().toISOString() && (
+        {post.content !== "" &&
+          new Date(post.metadata.createdAt) < new Date() && (
             <Suspense fallback={<span className="h-5 w-9"></span>}>
               <PageViews path={params.slug} />
             </Suspense>
-          ))}
+          )}
       </div>
 
       {post.content !== "" ? (
