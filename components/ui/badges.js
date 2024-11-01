8c7cc7f634ee5f46067f135af9ce2b656d17f978
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-11-01 12:29:09 +0100

    add project and update badge

--- components/ui/badges.js
+++ components/ui/badges.js
@@ -3,8 +3,10 @@ import { Link } from "next-view-transitions";
 
 import {
   ArchiveBoxIcon,
+  BriefcaseIcon,
   CursorArrowRaysIcon,
   MegaphoneIcon,
+  UserGroupIcon,
 } from "@heroicons/react/16/solid";
 
 const sizes = [
@@ -30,7 +32,7 @@ export function ExternalLink({ href, src, alt, text }) {
     <Link
       href={href}
       target="_blank"
-      className="inline-flex not-prose items-center rounded border border-zinc-200 bg-zinc-50 p-1 text-sm leading-4 text-zinc-700 no-underline dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-300"
+      className="inline-flex px-1.5 not-prose items-center rounded border border-zinc-200 bg-zinc-50 p-1 text-sm leading-4 text-zinc-700 no-underline dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-300"
     >
       <Image
         src={src}
@@ -67,7 +69,7 @@ export function ComingSoon({ className, size = "sm" }) {
   return (
     <div
       className={
-        "opacity-90 px-1 bg-blue-200/50 dark:bg-blue-800/50 text-blue-600 dark:text-blue-400 ring-1 ring-blue-500/50 rounded min-w-[20px] flex justify-center items-center gap-1 " +
+        "opacity-90 px-1.5 bg-blue-200/50 dark:bg-blue-800/50 text-blue-600 dark:text-blue-400 ring-1 ring-blue-500/50 rounded min-w-[20px] flex justify-center items-center gap-1 " +
         className
       }
     >
@@ -83,7 +85,7 @@ export function Archived({ className, size = "sm" }) {
   return (
     <div
       className={
-        "opacity-90 px-1 bg-orange-200/50 dark:bg-orange-800/50 text-orange-600 dark:text-orange-400 ring-1 ring-orange-500/50 rounded min-w-[20px] flex justify-center items-center gap-1 " +
+        "opacity-90 px-1.5 bg-orange-200/50 dark:bg-orange-800/50 text-orange-600 dark:text-orange-400 ring-1 ring-orange-500/50 rounded min-w-[20px] flex justify-center items-center gap-1 " +
         className
       }
     >
@@ -99,7 +101,7 @@ export function NewPost({ className, size = "sm" }) {
   return (
     <div
       className={
-        "opacity-90 px-1 bg-green-200/50 dark:bg-green-800/50 text-green-600 dark:text-green-400 ring-1 ring-green-500/50 rounded min-w-[20px] flex justify-center items-center gap-1 " +
+        "opacity-90 px-1.5 bg-green-200/50 dark:bg-green-800/50 text-green-600 dark:text-green-400 ring-1 ring-green-500/50 rounded min-w-[20px] flex justify-center items-center gap-1 " +
         className
       }
     >
@@ -108,6 +110,38 @@ export function NewPost({ className, size = "sm" }) {
       />
       <span className={sizes.find((s) => s.size === size)["text"]}>
         New Post
+      </span>
+    </div>
+  );
+}
+
+export function Project({ className, size = "sm" }) {
+  return (
+    <div
+      className={
+        "opacity-90 px-1.5 bg-zinc-200/50 dark:bg-zinc-800/50 text-zinc-600 dark:text-zinc-400 ring-1 ring-zinc-500/50 rounded min-w-[20px] flex justify-center items-center gap-1 " +
+        className
+      }
+    >
+      <BriefcaseIcon className={sizes.find((s) => s.size === size)["icon"]} />
+      <span className={sizes.find((s) => s.size === size)["text"]}>
+        Projects
+      </span>
+    </div>
+  );
+}
+
+export function Updates({ className, size = "sm" }) {
+  return (
+    <div
+      className={
+        "opacity-90 px-1.5 bg-zinc-200/50 dark:bg-zinc-800/50 text-zinc-600 dark:text-zinc-400 ring-1 ring-zinc-500/50 rounded min-w-[20px] flex justify-center items-center gap-1 " +
+        className
+      }
+    >
+      <UserGroupIcon className={sizes.find((s) => s.size === size)["icon"]} />
+      <span className={sizes.find((s) => s.size === size)["text"]}>
+        Updates
       </span>
     </div>
   );
