966717fc8eff87b0f927f48d0599555d6253af89
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2024-11-06 18:22:16 +0100

    revalidate every 12h

--- components/utils/getRepositories.js
+++ components/utils/getRepositories.js
@@ -1,7 +1,7 @@
 export default async function getRepositories() {
   const res = await fetch(
     "https://api.github.com/users/R4ULtv/repos?sort=created",
-    { next: { revalidate: 604800 } } // Revalidate every week
+    { next: { revalidate: 43200 } }, // Revalidate every 12h
   );
   return await res.json();
 }
