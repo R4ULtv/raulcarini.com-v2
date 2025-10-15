0006a76a60a2e1e991f26e71ae942ac4f42d556d
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-10-15 16:06:17 +0200

    feat: Add robots.txt generation for SEO optimization
    - Introduced a new robots.ts file to define rules for web crawlers.
    - Configured user agent rules to allow all access and specified the sitemap location for better search engine indexing.

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,11 @@
+import { baseURL } from "@/lib/url";
+
 export default function robots() {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.raulcarini.dev/sitemap.xml",
+    sitemap: `${baseURL}/sitemap.xml`,
   };
 }
