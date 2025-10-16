627571daf2b999c27c4d8af25381adaec65651ab
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-10-16 09:26:04 +0200

    Add blog and projects to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { getAllPosts } from "@/lib/content";
 import { baseURL } from "@/lib/url";
 
 export default async function sitemap() {
-  const posts = await getAllPosts()
+  const posts = await getAllPosts();
 
   return [
     {
@@ -10,6 +10,18 @@ export default async function sitemap() {
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 1,
+    },
+    {
+      url: `${baseURL}/blog`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
+    },
+    {
+      url: `${baseURL}/projects`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
     },
     ...posts.map((blog) => ({
       url: `${baseURL}/blog/${blog.slug}`,
